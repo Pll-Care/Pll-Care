790f154e660e3a93919d1589b97952f1203217e3
ghdtjgus76 <ghdtjgus76@naver.com>
2023-04-26 15:36:07 +0900

    Feat: Initial Settings for scss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import './css/project-full-care.css';
+
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <Login />
     </div>
   );
 }
