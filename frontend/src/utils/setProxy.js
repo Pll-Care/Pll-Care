40194243b66ec6b2a302b5ca7d3e325397e42734
ghdtjgus76 <ghdtjgus76@naver.com>
2023-04-27 17:49:09 +0900

    Fix: Modify target in proxy server setting

--- frontend/src/utils/setProxy.js
+++ frontend/src/utils/setProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api/v1", {
-      target: "http://localhost:8082",
+      target: "http://localhost:8080",
       changeOrigin: true,
     })
   );
