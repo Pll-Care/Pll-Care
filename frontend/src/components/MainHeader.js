8ed651e9b9ba876a98e0781c02b75fbdfd3ed631
ghdtjgus76 <ghdtjgus76@naver.com>
2023-05-18 17:41:41 +0900

    Style(ToggleMenu): Add interactive web feature to toggle menu button component

--- frontend/src/components/MainHeader.js
+++ frontend/src/components/MainHeader.js
@@ -4,11 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import Login from './Login/Login';
 import Button from './Button';
+import ToggleMenuButton from './ToggleMenuButton';
 
 import { authActions } from '../redux/authSlice';
 
 const MainHeader = () => {
     const [isLoginModalVisible, setIsLoginModalVisible] = useState(false);
+    const [isToggleMenuOpen, setIsToggleMenuOpen] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -35,6 +37,10 @@ const MainHeader = () => {
             <header className='main-header'>
                 <div className='main-header-left-col'>
                     <figure className='main-header-logo-img' />
+                    <ToggleMenuButton
+                        isToggleMenuOpen={isToggleMenuOpen}
+                        setIsToggleMenuOpen={setIsToggleMenuOpen}
+                    />
                 </div>
                 <div className='main-header-link'>
                     <Link to={'/management'}>프로젝트 관리</Link>
@@ -47,7 +53,10 @@ const MainHeader = () => {
                             type={'positive'}
                             onClick={handleLogout}
                         />
-                        <Link className='main-header-user-profile-img' to={'/profile'} />
+                        <Link
+                            className='main-header-user-profile-img'
+                            to={'/profile'}
+                        />
                     </div>
                 ) : (
                     <div className='main-header-right-col main-header-login-col'>
