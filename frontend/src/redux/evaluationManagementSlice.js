46839b0de18d76ee72a94145d7f2d78b165e8ee2
ghdtjgus76 <ghdtjgus76@naver.com>
2023-05-29 12:44:17 +0900

    Feat(EvaluationMangementSlice.js): Add redux file for evaluation management

---
+++ frontend/src/redux/evaluationManagementSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const evaluationManagementInitialState = {
+
+}
+
+const evaluationManagementSlice = createSlice({
+    name: 'evaluationManagement',
+    initialState: evaluationManagementInitialState,
+    reducers: {
+
+    }
+})
+
+export const evaluationManagementActions = evaluationManagementSlice.actions;
+
+export default evaluationManagementSlice;
