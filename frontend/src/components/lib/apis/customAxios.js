e89ac4641fe146fa6b62e9668faf995a50c9191e
ghdtjgus76 <ghdtjgus76@naver.com>
2023-06-14 12:03:26 +0900

    Fix(customAxios): Fix baseURL

--- frontend/src/components/lib/apis/customAxios.js
+++ frontend/src/components/lib/apis/customAxios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const accessToken = localStorage.getItem('access_token');
+const accessToken = localStorage.getItem('access_token') || '';
 
 export const customAxios = axios.create({
-    baseURL: 'https://localhost:8080',
+    baseURL: 'http://localhost:8080/api',
     headers: {
         'Authorization': `Bearer ${accessToken}`,
         'Content-Type': 'application/json'
