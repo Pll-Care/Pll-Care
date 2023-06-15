5a13bcc7e2554908115fdcab52e5ff79d73f9d0f
jeonghyun <invent819@naver.com>
2023-06-15 14:13:33 +0900

    평가 API 수정 (점수 type double to int & 예외)

--- backend/src/test/java/fullcare/backend/project/repository/ProjectRepositoryTest.java
+++ backend/src/test/java/fullcare/backend/project/repository/ProjectRepositoryTest.java
@@ -5,25 +5,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-@Transactional
-class ProjectRepositoryTest {
-    @Autowired
-    private ProjectRepository projectRepository;
-
-    @Test
-    public void ListQuery(){
-//        List<Project> byCreateDt = projectRepository.findByCreateDt(1l);
-//        for (Project project : byCreateDt) {
-//            System.out.println("project = " + project);
-//        }
-//        CustomPageRequest customPageRequest = new CustomPageRequest();
-//        PageRequest of = customPageRequest.of();
-//        Pageable pa = (Pageable)of;
-//        Page<Project> pageProjects = projectRepository.findByCreateDt(pa, 1l);
-//        for (Project pageProject : pageProjects) {
-//            System.out.println("pageProject = " + pageProject);
-//        }
-
-    }
-}
+//@SpringBootTest
+//@Transactional
+//class ProjectRepositoryTest {
+//    @Autowired
+//    private ProjectRepository projectRepository;
+//
+//    @Test
+//    public void ListQuery(){
+////        List<Project> byCreateDt = projectRepository.findByCreateDt(1l);
+////        for (Project project : byCreateDt) {
+////            System.out.println("project = " + project);
+////        }
+////        CustomPageRequest customPageRequest = new CustomPageRequest();
+////        PageRequest of = customPageRequest.of();
+////        Pageable pa = (Pageable)of;
+////        Page<Project> pageProjects = projectRepository.findByCreateDt(pa, 1l);
+////        for (Project pageProject : pageProjects) {
+////            System.out.println("pageProject = " + pageProject);
+////        }
+//
+//    }
+//}
