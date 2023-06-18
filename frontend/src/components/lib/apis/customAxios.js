8f4a1ceebbd5cb759f0c9cff869b8920f0717491
ghdtjgus76 <ghdtjgus76@naver.com>
2023-06-18 19:03:43 +0900

    Fix: Update server url

--- frontend/src/components/lib/apis/customAxios.js
+++ frontend/src/components/lib/apis/customAxios.js
@@ -4,7 +4,7 @@ const accessToken = localStorage.getItem("access_token");
 
 export const customAxios = axios.create({
   baseURL:
-    "http://ec2-43-202-32-247.ap-northeast-2.compute.amazonaws.com:8080/api",
+    "http://ec2-54-180-193-41.ap-northeast-2.compute.amazonaws.com:8080/api",
   headers: {
     Authorization: `Bearer ${accessToken}`,
     "Content-Type": "application/json",
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-const baseURL = `http://ec2-43-202-32-247.ap-northeast-2.compute.amazonaws.com:8080`;
+const baseURL = `http://ec2-54-180-193-41.ap-northeast-2.compute.amazonaws.com:8080`;
 
 export const kakaoAuthUrl = baseURL + `/oauth2/authorization/kakao`;
 export const googleAuthUrl = baseURL + `/oauth2/authorization/google`;
