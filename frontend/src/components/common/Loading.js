7158b492a3ec64b9a742e58aaea287f266b16f50
ghdtjgus76 <ghdtjgus76@naver.com>
2023-06-22 13:28:08 +0900

    Remove: Remove loading indicator

--- frontend/src/components/common/Loading.js
+++
@@ -1,9 +0,0 @@
-import CircularProgress from "@mui/material/CircularProgress";
-
-export const Loading = () => {
-  return (
-    <div className="loading-indicator-container">
-      <CircularProgress className="loading-indicator" />
-    </div>
-  );
-};
