946118a1cd64bca2c2df639d5ea346cb139b65ff
ShinDongJae <sdhsdj6450@gmail.com>
2023-06-23 02:07:22 +0900

    Feat: set header Authorization after login

--- frontend/src/layout/GeneralLayout.jsx
+++ frontend/src/layout/GeneralLayout.jsx
@@ -1,14 +1,30 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import MainHeader from "../components/common/MainHeader";
-import { isToken } from "../utils/localstroageHandler";
-import { useDispatch } from "react-redux";
+import { getToken, isToken } from "../utils/localstroageHandler";
+import { useDispatch, useSelector } from "react-redux";
 import { authActions } from "../redux/authSlice";
+import { useRouter } from "../hooks/useRouter";
+import { customAxios } from "../lib/apis/customAxios";
 
 const GeneralLayout = ({ children }) => {
   const dispatch = useDispatch();
+  const { currentPath, routeTo } = useRouter();
+
+  const isLogin = useSelector((state) => state.auth.isLoggedIn);
 
   useEffect(() => {
+    const accessToken = getToken("access_token");
+    customAxios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${accessToken}`;
+  }, [isLogin]);
+
+  useEffect(() => {
+    if (currentPath !== "/" && !isToken("access_token")) {
+      routeTo("/");
+    }
+
     if (isToken("access_token") && isToken("refresh_token")) {
       dispatch(authActions.login());
     }
