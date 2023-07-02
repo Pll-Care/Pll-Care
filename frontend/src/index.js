24e35955e85482ffc9328a73b7b397b6981e9ea0
ghdtjgus76 <ghdtjgus76@naver.com>
2023-07-02 20:30:46 +0900

    Feat: Add redux-persist for project complete state

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
 
 import App from "./App";
-import store from "./redux/store";
+import store from "../src/redux/rootReducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const persistor = persistStore(store);
+
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
---
+++ frontend/src/redux/rootReducer.js
@@ -0,0 +1,30 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+
+import authSlice from "./authSlice";
+import meetingRecordManagementSlice from "./meetingRecordManagementSlice";
+import evaluationManagementSlice from "./evaluationManagementSlice";
+import projectManagementSlice from "./projectManagementSlice";
+
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["projectManagement"],
+};
+
+export const rootReducer = combineReducers({
+  auth: authSlice.reducer,
+  meetingRecordManagement: meetingRecordManagementSlice.reducer,
+  evaluationManagement: evaluationManagementSlice.reducer,
+  projectManagement: projectManagementSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export default store;
