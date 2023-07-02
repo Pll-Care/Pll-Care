fe24c3ed710446febbe81132de36dd1aea842cde
ghdtjgus76 <ghdtjgus76@naver.com>
2023-07-02 19:03:11 +0900

    Feat: Add loading indicator

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,35 @@
 import { RouterProvider } from "react-router-dom";
+import React from "react";
 import { routers } from "./router";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { QueryClient, QueryClientProvider } from "react-query";
+
+import { Loading } from "../src/components/common/Loading";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import "./scss/fullcare.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={routers} />
-      <ToastContainer autoClose={3000} position={"top-right"} hideProgressBar />
+      <React.Suspense fallback={<Loading />}>
+        <RouterProvider router={routers} />
+        <ToastContainer
+          autoClose={3000}
+          position={"top-right"}
+          hideProgressBar
+        />
+      </React.Suspense>
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
