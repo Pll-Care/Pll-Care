ceb452717a2947528b330dfdf448c1a84eb7e8eb
ghdtjgus76 <ghdtjgus76@naver.com>
2023-07-02 20:51:03 +0900

    Remove: Remove loading indicator in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,32 +4,18 @@ import { routers } from "./router";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-import { Loading } from "../src/components/common/Loading";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import "./scss/fullcare.css";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      suspense: true,
-    },
-  },
-});
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <React.Suspense fallback={<Loading />}>
-        <RouterProvider router={routers} />
-        <ToastContainer
-          autoClose={3000}
-          position={"top-right"}
-          hideProgressBar
-        />
-      </React.Suspense>
+      <RouterProvider router={routers} />
+      <ToastContainer autoClose={3000} position={"top-right"} hideProgressBar />
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
