8090dd335024ebdd697b2ed30de2fbfdf68d79aa
ghdtjgus76 <ghdtjgus76@naver.com>
2023-07-03 19:50:51 +0900

    Refactor(EvaluationManagement): Refactor AllParticpants component and add ParticipantItem component

--- frontend/src/components/EvaluationManagement/AllParticipants.js
+++ frontend/src/components/EvaluationManagement/AllParticipants.js
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
+import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 
-import Button from "../../components/common/Button";
 import FinalEvaluation from "./FinalEvaluation";
-import { getProjectId } from "../../utils/getProjectId";
+import ParticipantItem from "./ParticipantItem";
 
-import defaultImgUrl from "../../assets/project-default-img.jpg";
+import { getProjectId } from "../../utils/getProjectId";
+import { isCompleteProject } from "../../utils/isCompleteProject";
 
 import { getEvaluationMember } from "../../lib/apis/evaluationManagementApi";
-import { useSelector } from "react-redux";
-import { isCompleteProject } from "../../utils/isCompleteProject";
 
 const AllParticipants = () => {
   const [isFinalEvaluationVisible, setIsFinalEvaluationVisible] = useState("");
@@ -45,53 +44,13 @@ const AllParticipants = () => {
     <div className="evaluation-management-all-participants">
       <h1>참여자 보기</h1>
       <div className="evaluation-management-participants">
-        {memberList.map((item) => (
-          <div
-            key={item.id}
-            onClick={() => handleClickParticipant(item.badgeDtos, item.id)}
-            className="evaluation-management-participant"
-          >
-            <div className="evaluation-management-participant-left-col">
-              <figure
-                style={{
-                  backgroundImage: `url(${
-                    item.imageUrl ? item.imageUrl : defaultImgUrl
-                  })`,
-                }}
-                className="evaluation-management-user-profile"
-              />
-            </div>
-            <div className="evaluation-management-participant-right-col">
-              <div className="name-badge-wrapper">
-                <div>{item.name}</div>
-                <div className="evaluation-management-badges">
-                  <div className="badge-quantity-container">
-                    {item.badgeDtos?.map((badge, idx) =>
-                      badge.quantity ? (
-                        <div
-                          key={item.id}
-                          className={[
-                            `badge-quantity`,
-                            `badge-quantity_${idx}`,
-                          ].join(" ")}
-                        >
-                          <figure />
-                          <div>X{badge.quantity}</div>
-                        </div>
-                      ) : null
-                    )}
-                  </div>
-                </div>
-              </div>
-              {(isCompleted === "COMPLETE" && !item.finalEvalId) && (
-                <Button
-                  text={"최종 평가하기"}
-                  size={"small"}
-                  onClick={() => handleFinalEvaluationModal(item.name)}
-                />
-              )}
-            </div>
-          </div>
+        {memberList.map((member) => (
+          <ParticipantItem
+            member={member}
+            handleFinalEvaluationModal={handleFinalEvaluationModal}
+            handleClickParticipant={handleClickParticipant}
+            isCompleted={isCompleted}
+          />
         ))}
       </div>
       {isFinalEvaluationVisible && (
---
+++ frontend/src/components/EvaluationManagement/ParticipantItem.js
@@ -0,0 +1,60 @@
+import defaultImgUrl from "../../assets/project-default-img.jpg";
+
+import Button from "../../components/common/Button";
+
+const ParticipantItem = ({
+  member,
+  handleFinalEvaluationModal,
+  handleClickParticipant,
+  isCompleted,
+}) => {
+  return (
+    <div
+      key={member.id}
+      onClick={() => handleClickParticipant(member.badgeDtos, member.id)}
+      className="evaluation-management-participant"
+    >
+      <div className="evaluation-management-participant-left-col">
+        <figure
+          style={{
+            backgroundImage: `url(${
+              member.imageUrl ? member.imageUrl : defaultImgUrl
+            })`,
+          }}
+          className="evaluation-management-user-profile"
+        />
+      </div>
+      <div className="evaluation-management-participant-right-col">
+        <div className="name-badge-wrapper">
+          <div>{member.name}</div>
+          <div className="evaluation-management-badges">
+            <div className="badge-quantity-container">
+              {member.badgeDtos?.map((badge, idx) =>
+                badge.quantity ? (
+                  <div
+                    key={member.id}
+                    className={[`badge-quantity`, `badge-quantity_${idx}`].join(
+                      " "
+                    )}
+                  >
+                    <figure />
+                    <div>X{badge.quantity}</div>
+                  </div>
+                ) : null
+              )}
+            </div>
+          </div>
+        </div>
+        {isCompleted === "COMPLETE" && !member.finalEvalId && (
+          <Button
+            text={"최종 평가하기"}
+            size={"small"}
+            onClick={() => handleFinalEvaluationModal(member.name)}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ParticipantItem;
