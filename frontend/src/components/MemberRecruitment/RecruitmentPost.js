d966c725dffae6e15c5d9a72d17d5373c3c03420
suminhan123 <suminh324@gmail.com>
2023-07-08 00:27:00 +0900

    Design: Modify like component

--- frontend/src/components/MemberRecruitment/RecruitmentPost.js
+++ frontend/src/components/MemberRecruitment/RecruitmentPost.js
@@ -1,15 +1,17 @@
+import { Link, useParams } from "react-router-dom";
+
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+
 import react from "./img/react.png";
 import firebase from "./img/firebase.png";
 import vue from "./img/vue.png";
-import { Link, useParams } from "react-router-dom";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import { useState } from "react";
 
 const RecruitmentPost = () => {
   const { id } = useParams();
-  const [like, setLike] = useState(false);
+  const like = false;
   return (
-    <Link to={`/recruitment/${id}`}>
+    <Link to={`/recruitment/${id}`} className="recruitment-post-link">
       <div className="recruitment-post">
         <img
           src="https://cdn.pixabay.com/photo/2015/01/08/18/25/desk-593327_640.jpg"
@@ -24,13 +26,11 @@ const RecruitmentPost = () => {
         <div className="recruitment-post-project">
           <div className="recruitment-post-project-title">
             <h2>ABC 프로젝트</h2>
-            <FavoriteIcon
-              className={`post-icon ${like ? "liked" : ""}`}
-              onClick={(event) => {
-                event.stopPropagation();
-                setLike(!like);
-              }}
-            />
+            {like ? (
+              <FavoriteIcon className="post-icon" />
+            ) : (
+              <FavoriteBorderIcon className="post-icon" />
+            )}
           </div>
           <h3>모집 기간: ----</h3>
           <h3>모집 포지션: -----</h3>
