5b5c47873dfb40953a0988eb1620e077b51ef16a
ShinDongJae <sdhsdj6450@gmail.com>
2023-07-13 23:02:56 +0900

    Feat(Profile): 내가 모집, 좋아하는 프로젝트 컴포넌트 UI

---
+++ frontend/src/utils/optionData.js
@@ -0,0 +1,9 @@
+export const recruitSelect = [
+  { value: "ONGOING", title: "모집 중" },
+  { value: "COMPLETE", title: "모집 완료" },
+];
+
+export const sortSelect = [
+  { value: "NEWEST", title: "최신순" },
+  { value: "OLDEST", title: "오래된순" },
+];
