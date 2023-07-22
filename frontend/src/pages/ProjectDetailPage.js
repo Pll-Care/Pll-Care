1a08e30069a892ccfd5f7a1c2f9bf6d199ba9f4e
ghdtjgus76 <ghdtjgus76@naver.com>
2023-07-22 10:54:01 +0900

    Feat(ProjectManagement): Separate management tab showing for leader and member

--- frontend/src/pages/ProjectDetailPage.js
+++ frontend/src/pages/ProjectDetailPage.js
@@ -1,8 +1,10 @@
 import { Outlet, useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
+import { useQuery } from "react-query";
 
 import ManagementHeader from "../components/common/ManagementHeader";
 import { isToken } from "../utils/localstroageHandler";
+import { getIsLeaderData } from "../lib/apis/managementApi";
 
 const ProjectDetailPage = () => {
   const { id } = useParams();
@@ -13,9 +15,13 @@ const ProjectDetailPage = () => {
     !isToken("access_token") && navigate("/", { replace: true });
   }, []);
 
+  const { data: isLeader } = useQuery(["isProjectLeader", id], () =>
+    getIsLeaderData(id)
+  );
+
   return (
     <div className="management_project_layout">
-      <ManagementHeader id={id} />
+      <ManagementHeader id={id} isLeader={isLeader} />
       <Outlet />
     </div>
   );
