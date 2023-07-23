133a2550e0fa21f042c217c741561ac8d5c314fd
ghdtjgus76 <ghdtjgus76@naver.com>
2023-07-23 23:01:24 +0900

    Feat: Block access to not existing project in management page

--- frontend/src/pages/ProjectDetailPage.js
+++ frontend/src/pages/ProjectDetailPage.js
@@ -3,8 +3,12 @@ import { useEffect } from "react";
 import { useQuery } from "react-query";
 
 import ManagementHeader from "../components/common/ManagementHeader";
+
 import { isToken } from "../utils/localstroageHandler";
 import { getIsLeaderData } from "../lib/apis/managementApi";
+import { getProjectData } from "../lib/apis/projectManagementApi";
+
+import { toast } from "react-toastify";
 
 const ProjectDetailPage = () => {
   const { id } = useParams();
@@ -13,21 +17,38 @@ const ProjectDetailPage = () => {
 
   const location = useLocation();
 
-  useEffect(() => {
-    if (!isToken("access_token")) {
-      navigate("/", { replace: true });
-    } else {
-      const pathRegex = new RegExp(`/management/${id}$`);
-      
-      if (location.pathname.match(pathRegex)) {
-        navigate(`/management/${id}/overview`, { replace: true });
-      }
+  const { data } = useQuery(
+    ["managementProject", id],
+    () => getProjectData(id),
+    {
+      keepPreviousData: true,
     }
-  }, [id, navigate, location.pathname]);
-
-  const { data: isLeader } = useQuery(["isProjectLeader", id], () =>
-    getIsLeaderData(id)
   );
+
+  const { data: isLeader } = useQuery(
+    ["isProjectLeader", id],
+    () => getIsLeaderData(id),
+    {
+      keepPreviousData: true,
+    }
+  );
+
+  useEffect(() => {
+    !isToken("access_token") && navigate("/", { replace: true });
+  }, []);
+
+  useEffect(() => {
+    const pathRegex = new RegExp(`/management/${id}$`);
+
+    if (data && location.pathname.match(pathRegex)) {
+      navigate(`/management/${id}/overview`, { replace: true });
+    }
+  }, [id, navigate, location.pathname, data]);
+
+  if (!data) {
+    navigate(-1, { replace: true });
+    return null;
+  }
 
   return (
     <div className="management_project_layout">
