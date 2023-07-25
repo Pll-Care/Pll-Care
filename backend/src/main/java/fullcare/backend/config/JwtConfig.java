b986408303eb8316954b11b14f0931a5fedd93d9
jeonghyun <invent819@naver.com>
2023-07-26 02:46:03 +0900

    oauth baseUri 변경

--- backend/src/main/java/fullcare/backend/config/JwtConfig.java
+++ backend/src/main/java/fullcare/backend/config/JwtConfig.java
@@ -46,6 +46,7 @@ public class JwtConfig {
         http
                 .securityMatcher("/api/**")
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/login/oauth2/**", "/api/oauth2/authorization/**").hasAnyRole("ANONYMOUS")
                         .requestMatchers(HttpMethod.GET, "/api/auth/post/list", "/api/auth/post/{postId:[\\d+]}").hasAnyRole("USER", "ANONYMOUS")
                         .requestMatchers(HttpMethod.GET, "api/auth/util/techstack").hasAnyRole("USER", "ANONYMOUS")
                         .requestMatchers(HttpMethod.GET, "/api/auth/profile/{memberId:[\\d+]}/rolestack",
