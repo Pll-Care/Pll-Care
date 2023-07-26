ecdb5e137c646494d56ea6b1719c75bbe860a5d9
jeonghyun <invent819@naver.com>
2023-07-27 02:30:39 +0900

    시간 설정

--- backend/src/main/java/fullcare/backend/BackendApplication.java
+++ backend/src/main/java/fullcare/backend/BackendApplication.java
@@ -1,9 +1,12 @@
 package fullcare.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableWebSecurity(debug = true)
@@ -11,6 +14,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class BackendApplication {
     static {
         System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
     public static void main(String[] args) {
