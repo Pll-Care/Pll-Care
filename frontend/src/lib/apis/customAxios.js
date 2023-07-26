4def883d7ae2431aae7ef26a684dd930fbf127aa
ShinDongJae <sdhsdj6450@gmail.com>
2023-07-26 13:55:38 +0900

    fix: 서버 URL 변경

--- frontend/src/lib/apis/customAxios.js
+++ frontend/src/lib/apis/customAxios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const accessToken = localStorage.getItem("access_token");
 
 export const customAxios = axios.create({
-  baseURL:
-    "http://ec2-54-180-193-41.ap-northeast-2.compute.amazonaws.com:8080/api",
+  baseURL: "https://fullcare.store/api",
   headers: {
     "Content-Type": "application/json",
   },
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
-const baseURL = `http://ec2-54-180-193-41.ap-northeast-2.compute.amazonaws.com:8080`;
+const BASE_URL = `https://fullcare.store/api`;
 
-export const kakaoAuthUrl = baseURL + `/oauth2/authorization/kakao`;
-export const googleAuthUrl = baseURL + `/oauth2/authorization/google`;
-export const naverAuthUrl = baseURL + `/oauth2/authorization/naver`;
+export const kakaoAuthUrl = BASE_URL + `/oauth2/authorization/kakao`;
+export const googleAuthUrl = BASE_URL + `/oauth2/authorization/google`;
+export const naverAuthUrl = BASE_URL + `/oauth2/authorization/naver`;
