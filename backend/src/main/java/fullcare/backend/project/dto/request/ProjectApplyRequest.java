4b4a3cc236eda59aca6f8fbe6afe06f9d61f2571
jeonghyun <invent819@naver.com>
2023-08-08 18:49:27 +0900

    에러 수정

--- backend/src/main/java/fullcare/backend/project/dto/request/ProjectApplyRequest.java
+++ backend/src/main/java/fullcare/backend/project/dto/request/ProjectApplyRequest.java
@@ -1,6 +1,7 @@
 package fullcare.backend.project.dto.request;
 
 import fullcare.backend.projectmember.domain.ProjectMemberPositionType;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class ProjectApplyRequest {
-
+    @NotNull
     private ProjectMemberPositionType position;
     
     // ! 테스트 데이터용 생성자
