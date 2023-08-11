1104159b999fa78a1e3f155338064cfc2faf20c8
ShinDongJae <sdhsdj6450@gmail.com>
2023-08-12 00:22:31 +0900

    feat(TeamMemberManagement): 팀원 관리 이미지 클릭 시 유저 프로필로 이동 처리

--- frontend/src/components/TeamMemberManagement/MemberItem.jsx
+++ frontend/src/components/TeamMemberManagement/MemberItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import profile_default from "../../assets/profile-default-img.png";
 import EditTeamMember from "./EditTeamMember";
+import { Link } from "react-router-dom";
 
 const MemberItem = ({
   name,
@@ -14,19 +15,21 @@ const MemberItem = ({
     <li className="member_item">
       <div className="member_item_imgbox">
         <div>
-          <img
-            src={imageUrl ? imageUrl : profile_default}
-            alt={`${name}의 프로필`}
-            className="member_item_imgbox_img"
-          />
-          {isEdit && (
-            <EditTeamMember
-              memberId={memberId}
-              refetch={refetch}
-              name={name}
-              position={position}
+          <Link to={`/profile/${memberId}/introduce`}>
+            <img
+              src={imageUrl ? imageUrl : profile_default}
+              alt={`${name}의 프로필`}
+              className="member_item_imgbox_img"
             />
-          )}
+            {isEdit && (
+              <EditTeamMember
+                memberId={memberId}
+                refetch={refetch}
+                name={name}
+                position={position}
+              />
+            )}
+          </Link>
         </div>
       </div>
       <div className="member_item_infobox">
@@ -38,8 +41,3 @@ const MemberItem = ({
 };
 
 export default React.memo(MemberItem);
-
-/*
-TODO: 팀 관리 부분은 프로젝트를 만든 사람이 아니면 못들어가게 만들어야 할거 같다
-TODO: 프로젝트에 대한 권한이 없습니다 나오면 프로젝트 관리 페이지로 이동하도록 만들기
-*/
