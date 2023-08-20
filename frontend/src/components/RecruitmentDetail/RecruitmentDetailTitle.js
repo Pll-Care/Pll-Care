72e2d2d4a07efa30a200a7e9abf0e452bac80b37
suminhan123 <suminh324@gmail.com>
2023-08-20 11:38:00 +0900

    Design(Recruitment): Apply mobile fonts to recruitment detail page

--- frontend/src/components/RecruitmentDetail/RecruitmentDetailTitle.js
+++ frontend/src/components/RecruitmentDetail/RecruitmentDetailTitle.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import { useMediaQuery } from "@mui/material";
 
 import projectDefaultImg from "../../assets/project-default-img.jpg";
 import { getStringDate } from "../../utils/date";
+import { query } from "../../utils/mediaQuery";
 import Button from "../common/Button";
 
 const RecruitmentDetailTitle = ({
@@ -17,6 +19,8 @@ const RecruitmentDetailTitle = ({
   setDeleteIsModalVisible,
 }) => {
   const time = data ? getStringDate(new Date(data.createdDate)) : "";
+
+  const isMobile = useMediaQuery(query);
 
   // 수정 취소 버튼 눌렀을 때
   const handleCancelModifyPost = () => {
@@ -38,9 +42,11 @@ const RecruitmentDetailTitle = ({
   return (
     <div className="detail-title">
       <div className="detail-title-content">
-        <Link to="/recruitment" className="mui-arrow">
-          <ArrowBackIosNewIcon />
-        </Link>
+        {isMobile && !isEdit && (
+          <Link to="/recruitment" className="mui-arrow">
+            <ArrowBackIosNewIcon />
+          </Link>
+        )}
 
         {data?.projectImageUrl ? (
           <img src={data?.projectImageUrl} alt="" />
@@ -60,7 +66,7 @@ const RecruitmentDetailTitle = ({
         ) : (
           <h1>{data?.title}</h1>
         )}
-        <h2>{time} 작성</h2>
+        {!isEdit && <h2>{time} 작성</h2>}
       </div>
       <div className="detail-title-button">
         {isEdit ? (
