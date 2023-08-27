3a21bb4c6851da9f2033a11da1931dc77c9b67a4
ghdtjgus76 <ghdtjgus76@naver.com>
2023-08-27 23:38:57 +0900

    Fix: Fix error about isLeaderData api call

--- frontend/src/context/ProjectDetailContext.jsx
+++ frontend/src/context/ProjectDetailContext.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { createContext, useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { useRouter } from "../hooks/useRouter";
 import { getIsLeaderData } from "../lib/apis/managementApi";
+import { toast } from "react-toastify";
 
 const ProjectDetailContext = createContext(null);
 export const useProjectDetail = () => useContext(ProjectDetailContext);
@@ -9,12 +11,20 @@ export const useProjectDetail = () => useContext(ProjectDetailContext);
 export function ProjectDetailProvider({ children }) {
   const [isLeader, setIsLeader] = useState(false);
   const { id: projectId } = useParams();
+  const { routeOptionTo } = useRouter();
 
   useEffect(() => {
     const getIsLeader = async () => {
-      const isLeaderData = await getIsLeaderData(projectId);
-
-      setIsLeader((_) => isLeaderData);
+      try {
+        const response = await getIsLeaderData(projectId);
+        if (response.status === 200) setIsLeader((_) => response.data.leader);
+      } catch (error) {
+        if (error.response.data.status === 404) {
+          toast.error(error.response.data.message);
+          routeOptionTo("/management", { replace: true });
+        }
+        console.log(error);
+      }
     };
 
     getIsLeader();
