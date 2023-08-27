77e7c03d7c1e192c56639b60c1c71e3afb1a2402
홍서현 <81316050+ghdtjgus76@users.noreply.github.com>
2023-08-27 23:41:33 +0900

    Revert "isLeader api 호출 부분 수정 (#72)"
    This reverts commit 0817d7b1e113fc3276e345d3bfb87b7afac299ce.

--- frontend/src/context/ProjectDetailContext.jsx
+++ frontend/src/context/ProjectDetailContext.jsx
@@ -1,9 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { createContext, useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { useRouter } from "../hooks/useRouter";
 import { getIsLeaderData } from "../lib/apis/managementApi";
-import { toast } from "react-toastify";
 
 const ProjectDetailContext = createContext(null);
 export const useProjectDetail = () => useContext(ProjectDetailContext);
