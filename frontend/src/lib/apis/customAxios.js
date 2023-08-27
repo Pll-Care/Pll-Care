316260951e7279d6a5a042cffcc1059858d829c1
ghdtjgus76 <ghdtjgus76@naver.com>
2023-08-27 21:15:19 +0900

    Fix(Login): Fix error about token

--- frontend/src/lib/apis/customAxios.js
+++ frontend/src/lib/apis/customAxios.js
@@ -1,25 +1,8 @@
 import axios from "axios";
 
-import { toast } from "react-toastify";
-
-const accessToken = localStorage.getItem("access_token");
-
 export const customAxios = axios.create({
   baseURL: "https://fullcare.store/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-customAxios.defaults.headers.common["Authorization"] = accessToken
-  ? `Bearer ${accessToken}`
-  : null;
-
-// customAxios.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   (error) => {
-
-//   }
-// )
