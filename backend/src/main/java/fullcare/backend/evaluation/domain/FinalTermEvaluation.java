d920e32f2721285c437decf55c1cb92920e06fc6
jeonghyun <invent819@naver.com>
2023-08-29 18:13:50 +0900

    DDL TINYTEXT -> TEXT로 변경

--- backend/src/main/java/fullcare/backend/evaluation/domain/FinalTermEvaluation.java
+++ backend/src/main/java/fullcare/backend/evaluation/domain/FinalTermEvaluation.java
@@ -22,8 +22,7 @@ public class FinalTermEvaluation {
     @Column(name = "final_eval_id")
     private Long id;
 
-    @Lob
-    @Column(name = "content", nullable = false)
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @Embedded
--- backend/src/main/java/fullcare/backend/memo/domain/Memo.java
+++ backend/src/main/java/fullcare/backend/memo/domain/Memo.java
@@ -34,8 +34,8 @@ public class Memo extends BaseEntity {
     @Column(name = "title", nullable = false)
     private String title;
 
-    @Lob
-    @Column(name = "content", nullable = false)
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @OneToMany(mappedBy = "memo", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
