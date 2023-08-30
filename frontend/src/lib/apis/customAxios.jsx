bb0a510834e86eff9412e4a01c9766a1545069ea
홍서현 <81316050+ghdtjgus76@users.noreply.github.com>
2023-08-30 20:55:29 +0900

    Fix: Fix error about axios header token null (#83)

--- frontend/src/lib/apis/customAxios.jsx
+++ frontend/src/lib/apis/customAxios.jsx
@@ -7,6 +7,12 @@ export const customAxios = axios.create({
   },
 });
 
+const accessToken = localStorage.getItem("access_token");
+
+customAxios.defaults.headers.common["Authorization"] = accessToken
+  ? `Bearer ${accessToken}`
+  : null;
+
 // customAxios.interceptors.response.use(
 //   (response) => {
 //     return response;
