e5c6debd314a0908e725a51710b3277764d490b4
PYOSESEUNG <97ss93sh@gmail.com>
2023-09-01 19:17:11 +0900

    토큰 재발급용 API 추가 및 관련 코드 수정

--- backend/src/main/java/fullcare/backend/security/jwt/exception/JwtErrorCode.java
+++ backend/src/main/java/fullcare/backend/security/jwt/exception/JwtErrorCode.java
@@ -14,7 +14,8 @@ public enum JwtErrorCode implements ErrorCode {
     UNSUPPORTED_TOKEN("JWT_003", HttpStatus.UNAUTHORIZED, "지원되지 않는 JWT 서명입니다."),
     ILLEGAL_TOKEN("JWT_004", HttpStatus.UNAUTHORIZED, "JWT 토큰이 잘못되었습니다."),
     NOT_FOUND_USER("JWT_005", HttpStatus.UNAUTHORIZED, "등록되지 않은 사용자입니다."),
-    // ! refresh token 도 만료되어 재 로그인이 필요한 경우
+    
+    // ! refresh token 도 만료되어 재 로그인이 필요한 경우 -> jwt_002로 처리?
     AUTHENTICATION_FAIL("JWT_006", HttpStatus.UNAUTHORIZED, "재 로그인이 필요합니다."),
 
     // ! 토큰이 없다 -> 인증 실패
