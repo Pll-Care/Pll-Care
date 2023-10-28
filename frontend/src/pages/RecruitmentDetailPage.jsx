f03b98570db897115b7fabfe43d73e68474c6f67
suminhan123 <suminh324@gmail.com>
2023-10-29 08:43:56 +0900

    모집글 api 에러 수정

--- frontend/src/pages/RecruitmentDetailPage.jsx
+++ frontend/src/pages/RecruitmentDetailPage.jsx
@@ -1,11 +1,22 @@
 import RecruitmentDetailContent from "../components/RecruitmentDetail";
+import { useGeneralClient } from "../context/Client/GeneralClientContext";
+import ProfileClientProvider from "../context/Client/ProfileClientContext";
+import { ProfileProvider } from "../context/ProfileContext";
 import RecruitmentLayout from "../layout/RecruitmentLayout";
+import ProfileService from "../lib/service/ProfileService";
 
 const RecruitmentDetailPage = () => {
+  const { generalHttpClient } = useGeneralClient();
+
+  const profileService = new ProfileService(generalHttpClient);
   return (
-    <RecruitmentLayout>
-      <RecruitmentDetailContent />
-    </RecruitmentLayout>
+    <ProfileClientProvider profileService={profileService}>
+      <ProfileProvider>
+        <RecruitmentLayout>
+          <RecruitmentDetailContent />
+        </RecruitmentLayout>
+      </ProfileProvider>
+    </ProfileClientProvider>
   );
 };
 export default RecruitmentDetailPage;
--- frontend/src/pages/RecruitmentWritePage.jsx
+++ frontend/src/pages/RecruitmentWritePage.jsx
@@ -1,11 +1,23 @@
 import MemberRecruitmentWrite from "../components/RecruitmentWrite";
+import { useGeneralClient } from "../context/Client/GeneralClientContext";
+import ProfileClientProvider from "../context/Client/ProfileClientContext";
+import { ProfileProvider } from "../context/ProfileContext";
 import RecruitmentLayout from "../layout/RecruitmentLayout";
+import ProfileService from "../lib/service/ProfileService";
 
 const RecruitmentWritePage = () => {
+  const { generalHttpClient } = useGeneralClient();
+
+  const profileService = new ProfileService(generalHttpClient);
+
   return (
-    <RecruitmentLayout>
-      <MemberRecruitmentWrite />
-    </RecruitmentLayout>
+    <ProfileClientProvider profileService={profileService}>
+      <ProfileProvider>
+        <RecruitmentLayout>
+          <MemberRecruitmentWrite />
+        </RecruitmentLayout>
+      </ProfileProvider>
+    </ProfileClientProvider>
   );
 };
 export default RecruitmentWritePage;
